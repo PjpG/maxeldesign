f42dfef57c1de33595252ff15f7c6b33959ca649
PjpG <patryk@doppio.jp>
2012-08-02 11:06:22 +0400

    First version of page done.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_filter :authenticate
+
+  private
+    def authenticate
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "admin" && password == "maxeldesign"
+      end
+    end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maxeldesign::Application.config.secret_token = 'f426e67cbf0207c2a6ea780fa9034dc4470ab27701b5acd0fbaae365c700443322a24e9cb47fc7aff313b1d50eba422e5cff362f492534fe371b6b660e9d2323'
